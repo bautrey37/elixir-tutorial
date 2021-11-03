f0aa44315d951236a442d54302ca2c7a3229e114
Brandon Autrey <brandon.autrey@glia.com>
2021-11-03 23:02:58 +0200

    Finish asynchronous tasks section

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -1,32 +1,27 @@
 defmodule HttpServerTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
 
   alias Servy.HttpServer
-  alias Servy.HttpClient
 
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
-    max_concurrent_requests = 5
+    urls = [
+      "http://localhost:4000/wildthings",
+      "http://localhost:4000/sensors",
+      "http://localhost:4000/bears",
+      "http://localhost:4000/bears/1",
+      "http://localhost:4000/api/bears"
+    ]
 
-    parent = self()
+    urls
+    |> Enum.map(fn(url) -> Task.async(fn -> HTTPoison.get(url) end) end)
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(&assert_successful_response/1)
+  end
 
-    for _ <- 1..max_concurrent_requests do
-      spawn(fn ->
-        # Send the request
-        {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
-
-        # Send the response back to the parent
-        send(parent, {:ok, response})
-      end)
-    end
-
-    for _ <- 1..max_concurrent_requests do
-      receive do
-        {:ok, response} ->
-          assert response.status_code == 200
-          assert response.body == "Bears, Lions, Tigers"
-      end
-    end
+  defp assert_successful_response({:ok, response}) do
+    assert response.status_code == 200
+    # assert response.body == "Bears, Lions, Tigers"
   end
 end
