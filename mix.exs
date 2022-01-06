5db1f5679c7573c641fc8b80970d39cf9d9ea586
Brandon Autrey <brandon.autrey@glia.com>
2022-01-07 00:37:50 +0200

    Upgrade to elixir 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Servy.MixProject do
     [
       app: :servy,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
